fe83abb2ffb84fc1d80bcff6930d4792eada834f
FernandoMarcos28 <shonando27@gmail.com>
2019-09-09 17:49:03 -0300

    Ajustado mais os gráficos

--- src/Sobre.js
+++ src/Sobre.js
@@ -17,22 +17,32 @@ class Sobre extends Component{
                     },
                     {type: 'bar', x: [1, 2, 3], y: [2, 5, 3]},
                     ]}
-                    layout={ {width: 500, height: 500, title: 'A Fancy Plot'} }
+                    layout={ {width: 500, height: 500, title: 'A Fancy Plot',plot_bgcolor:"#7F7F7F",paper_bgcolor:"#FFF3"} }
+		    onClick={(data) => {
+		    	  let pts= '';
+			  for(let i=0; i <  data.points.length; i++){
+			    pts = 'x = '+data.points[i].x + '\ny = '+
+			    data.points[i].y.toPrecision(4) + '\n\n';
+			  }
+			  alert('Clorest point clicked:\n\n'+pts);
+		    	}
+		    }
                 />
                 </div>
                 <div className="col-lg-6">
                 <Plot
                     data={[
                     {
-                        x: [1, 2, 3],
+                        x: ['2019-06-04 22:23:00','2019-06-06 22:23:00','2019-06-24 22:23:00'],
                         y: [2, 6, 3],
                         type: 'scatter',
                         //mode: 'lines+points',
                         marker: {color: 'red'},
                     },
-                    {type: 'line', x: [1, 2, 3], y: [2, 5, 3]},
+                    {type: 'line', x: ['2019-06-04 22:23:00','2019-06-06 22:23:00','2019-06-24 22:23:00'], y: [2, 5, 3]},
                     ]}
-                    layout={ {width: 500, height: 500, title: 'A Fancy Plot'} }
+                    layout={ {title: 'A Fancy Plot',width: 500, height: 500} }
+		     onClick={(data) => console.log(this.props.width)}
                 />
                 </div>
 		   <div className="col-lg-6">
