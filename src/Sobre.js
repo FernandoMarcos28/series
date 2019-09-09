6c6ff4a34f0ac0afa941d240d5db94de79abd4ca
FernandoMarcos28 <shonando27@gmail.com>
2019-09-09 14:29:12 -0300

    Adicionado o gráfico de pizza.

--- src/Sobre.js
+++ src/Sobre.js
@@ -4,8 +4,8 @@ import Plot from 'react-plotly.js';
 class Sobre extends Component{
     render(){
         return (
-            <div className="col-lg-6">
-                <div >
+            <div className="col-lg-12">
+                <div className="col-lg-6" >
                 <Plot
                     data={[
                     {
@@ -33,6 +33,19 @@ class Sobre extends Component{
                     {type: 'line', x: [1, 2, 3], y: [2, 5, 3]},
                     ]}
                     layout={ {width: 500, height: 500, title: 'A Fancy Plot'} }
+                />
+                </div>
+		   <div className="col-lg-6">
+                <Plot
+                    data={[
+                    {
+                        values: [19, 26, 55],
+                        type: 'pie',
+			labels: ['Residential','Non-Residential','Utility']
+                        //mode: 'lines+points',
+                    },
+                    ]}
+                    layout={ { title: 'A Fancy Plot'},{responsive: true} }
                 />
                 </div>
              </div>
